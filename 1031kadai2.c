93659c68576b564f03d4f9898c95fbe9960c73af
yutaro0327 <t246550@st.yamagata-u.ac.jp>
2025-10-31 19:21:14 +0900

    Add files via upload

---
+++ 1031kadai2.c
@@ -0,0 +1,80 @@
+
+
+
+int main()
+{
+    int k;
+    printf("ボールは何個ありますか? ");
+    scanf("%d", &k);
+
+    if (k >= 100)
+    {
+        printf("たくさんのボール\n");
+    }
+    else if (k == 0)
+    {
+        printf("ボールはどこにやったの?\n");
+    }
+    else
+    {
+        printf("少しのボール\n");
+    }
+
+    return 0;
+}
+
+
+int main()
+{
+    int k;
+    printf("ペンは何個ありますか? ");
+    scanf("%d", &k);
+
+    if (k >= 10)
+    {
+        printf("たくさんのペン\n");
+    }
+    else if (k == 0)
+    {
+        printf("ペンはどこにやったの?\n");
+    }
+    else
+    {
+        printf("少しのペン\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
