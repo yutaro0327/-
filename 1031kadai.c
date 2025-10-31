296c81ee3947b3fcd84f384ab223c961f89d2335
yutaro0327 <t246550@st.yamagata-u.ac.jp>
2025-10-31 18:38:55 +0900

    Add files via upload

---
+++ 1031kadai.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int diff;
+    char large; //大文字
+    char small; //小文字
+    
+    printf("アルファベットの小文字を入力してください>>>");
+    scanf("%c", &small);
+　　diff = 'A' - 'a';      
+　　large = small - diff;
+　　
+    printf("小文字は%c 大文字は%c\n", small, large);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
